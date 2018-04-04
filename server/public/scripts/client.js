6915e5459d740584ccd9519c3f0dfb68ae19a1b0
Blake Burbach <cras.bburbach@gmail.com>
2018-04-04 18:33:33 -0500

    File structure finished, index and client started.

---
+++ server/public/scripts/client.js
@@ -0,0 +1,23 @@
+const TimeTrackerApp = angular.module('TimerTrackerApp', ['ngRoute']);
+
+TimeTrackerApp.config(['$routeProvider', function(){
+    console.log('config loaded');
+    let self = this;
+
+    $routeProvider
+        .when('/', {
+            redirectTo: '/entries'
+        })
+        .when('/entries', {
+            templateUrl: '/views/entries.html',
+            controller: 'EntriesController as vm'
+        })
+        .when('/projects', {
+            templateUrl: '/views/projects.html',
+            controller: 'ProjectsController as vm'
+        })
+        .otherwise({
+            template: '<h1>404 Not Found</h1>'
+        });
+
+}]);
