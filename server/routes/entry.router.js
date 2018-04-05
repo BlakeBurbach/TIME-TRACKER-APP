43a9b5dc9943a56387d4dc2734a8f9dda0241480
Blake Burbach <cras.bburbach@gmail.com>
2018-04-05 16:02:03 -0500

    got client side to send a time stamp to database

--- server/routes/entry.router.js
+++ server/routes/entry.router.js
@@ -6,9 +6,9 @@ const pool = require('../modules/pool.js');
 router.post('/', (req, res) => {
     let entryToAdd = req.body;
     // add user's entry data to time_tracker database under project_entries
-    let queryText = `INSERT INTO project_entries (description, start_date, start_time, end_date, end_time)
-                     VALUES ($1, $2, $3, $4, $5);`
-    pool.query(queryText, [entryToAdd.description, entryToAdd.start_date, entryToAdd.start_time, entryToAdd.end_date, entryToAdd.end_time])
+    let queryText = `INSERT INTO project_entries (description, start_date, end_date)
+                     VALUES ($1, $2, $3);`
+    pool.query(queryText, [entryToAdd.description, entryToAdd.start_date, entryToAdd.end_date])
     .then( (result) => {
         console.log('project_entry router POST success', result);
         res.sendStatus(200);
