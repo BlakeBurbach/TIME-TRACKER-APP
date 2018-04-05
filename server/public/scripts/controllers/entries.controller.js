404ad64a46634884431b98f27af9d9d3a764c3b8
Blake Burbach <cras.bburbach@gmail.com>
2018-04-05 11:11:55 -0500

    isntalled angular moment

--- server/public/scripts/controllers/entries.controller.js
+++ server/public/scripts/controllers/entries.controller.js
@@ -1,11 +1,15 @@
 TimeTrackerApp.controller('EntriesController', ['TimeTrackerService', function(TimeTrackerService){
     console.log('EntriesController loaded');
     let self = this;
-    
-    self.newEntry = {description: '', date: '', startTime: '', EndTime: ''}
+
+    // self.newEntry.date = moment(self.newEntry.date).format("DD-MM-YYYY")
+    self.newEntry = {description: '', date: '', startTime: '', EndTime: ''};
+    // console.log(self.newEntry.date);
 
     self.addEntry = function(entryToAdd){
         TimeTrackerService.addEntry(entryToAdd);
         self.newEntry = {description: '', date: '', startTime: '', EndTime: ''};
+        self.newEntry.date = moment(self.newEntry.date).format("MM-DD-YYYY");
+        console.log(self.newEntry.date);
     }
 }]);
