0a1d95a3da6a1127a548886f950061aafe953056
Blake Burbach <cras.bburbach@gmail.com>
2018-04-05 10:59:04 -0500

    sourced in moment.js and will try to be converting dates and times to strings for database

--- server/public/scripts/controllers/entries.controller.js
+++ server/public/scripts/controllers/entries.controller.js
@@ -1,6 +1,11 @@
 TimeTrackerApp.controller('EntriesController', ['TimeTrackerService', function(TimeTrackerService){
     console.log('EntriesController loaded');
     let self = this;
+    
+    self.newEntry = {description: '', date: '', startTime: '', EndTime: ''}
 
-    self.addEntry = TimeTrackerService.addEntry;
+    self.addEntry = function(entryToAdd){
+        TimeTrackerService.addEntry(entryToAdd);
+        self.newEntry = {description: '', date: '', startTime: '', EndTime: ''};
+    }
 }]);
