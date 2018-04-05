2c5c2edb7a0df2da3430135f02e76b082288f4c1
Blake Burbach <cras.bburbach@gmail.com>
2018-04-05 15:16:23 -0500

    going to try and post to SQL

--- server/public/scripts/controllers/entries.controller.js
+++ server/public/scripts/controllers/entries.controller.js
@@ -2,18 +2,8 @@ TimeTrackerApp.controller('EntriesController', ['TimeTrackerService', 'moment', 
     console.log('EntriesController loaded');
     let self = this;
 
-    // self.newEntry.date = moment(self.newEntry.date).format("DD-MM-YYYY")
-    self.newEntry = {description: '', date: '', startTime: '', endTime: ''};
-
     self.addEntry = function(entryToAdd){
+        console.log('in addEntry', entryToAdd);
         TimeTrackerService.addEntry(entryToAdd);
-        let newTime = self.newEntry.endTime - self.newEntry.startTime;
-        
-        self.newEntry = {
-            description: '',
-            date: '',
-            startTime: '',
-            endTime: ''};
     }
-    // console.log(self.newEntry);
 }]);
