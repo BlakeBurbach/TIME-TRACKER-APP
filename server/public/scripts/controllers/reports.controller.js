fc232f1ecb5b1139cd516ea398241304ec4c3f33
Blake Burbach <cras.bburbach@gmail.com>
2018-04-08 16:25:33 -0500

    added chartjs to reports html and can display a chart of total time per client

--- server/public/scripts/controllers/reports.controller.js
+++ server/public/scripts/controllers/reports.controller.js
@@ -1,3 +1,57 @@
 TimeTrackerApp.controller('ReportsController', ['TimeTrackerService', function(TimeTrackerService){
     console.log('ReportsController loaded');
+    let self = this;
+
+    self.generateGraph = false;
+    self.projectList = TimeTrackerService.projectList;
+
+    self.makeChart = function () {
+        let projectList = self.projectList.list
+        console.log(projectList);
+        let clientArray = projectList.map((e, i) => {
+            return e.client; 
+        })
+        let hoursArray = projectList.map((e, i) => {
+            return e.total_time; 
+        })
+    
+        var ctx = angular.element( document.querySelector( '#myChart' ) );
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: clientArray,
+                datasets: [{
+                    label: 'Billable Hours by Client',
+                    data: hoursArray,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+        
+    }
 }]);
