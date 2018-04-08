964bcc8ab0e0e8c14f16a6f3587ab6b531356199
Blake Burbach <cras.bburbach@gmail.com>
2018-04-08 17:34:44 -0500

    added another chart to reports html

--- server/public/scripts/controllers/reports.controller.js
+++ server/public/scripts/controllers/reports.controller.js
@@ -6,6 +6,7 @@ TimeTrackerApp.controller('ReportsController', ['TimeTrackerService', function(T
     self.projectList = TimeTrackerService.projectList;
 
     self.makeChart = function () {
+        console.log('in makeChart');
         let projectList = self.projectList.list
         console.log(projectList);
         let clientArray = projectList.map((e, i) => {
@@ -15,8 +16,8 @@ TimeTrackerApp.controller('ReportsController', ['TimeTrackerService', function(T
             return e.total_time; 
         })
     
-        var ctx = angular.element( document.querySelector( '#myChart' ) );
-        var myChart = new Chart(ctx, {
+        let ctx = angular.element( document.querySelector( '#myChart' ) );
+        let myChart = new Chart(ctx, {
             type: 'bar',
             data: {
                 labels: clientArray,
@@ -52,6 +53,55 @@ TimeTrackerApp.controller('ReportsController', ['TimeTrackerService', function(T
                 }
             }
         });
-        
+    }
+
+    self.makeSecondChart = function(){
+        console.log('in makeSecondChart');
+        let projectList = self.projectList.list
+
+        let clientArray = projectList.map((e, i) => {
+            return e.client; 
+        })
+        let moneyArray = projectList.map((e, i) => {
+            return ('$', e.total_earnings); 
+        })
+    
+        let ctx = angular.element( document.querySelector( '#mySecondChart' ) );
+        let mySecondChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: clientArray,
+                datasets: [{
+                    label: 'Total Earnings by Client',
+                    data: moneyArray,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
     }
 }]);
